2a03c241762d3a84514884c86ecbaef65a743293
vhs21 <uladzislau.shamionak@gmail.com>
2018-10-15 18:29:23 +0300

    tf-idf

--- toxicity.R
+++ toxicity.R
@@ -1,6 +1,15 @@
 library(readr)
 library(tidyverse)
 library(tidytext)
+library(ggplot2)
+
 
 train_data <- read_csv('data/train.csv')
 
+train_words <- train_data %>%
+	unnest_tokens(word, comment_text) %>%
+	count(id, word, sort = TRUE) %>%
+	bind_tf_idf(word, id, n)
+
+ggplot(data = train_words) +
+	geom_point(mapping = aes(x = n, y = tf_idf))
